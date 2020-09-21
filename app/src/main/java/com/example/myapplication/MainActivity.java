3cfe415d9f6abc325f1c418467872978b878a61b
295522894 <kevin52072@hotmail.com>
2020-09-21 09:23:57 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,8 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ReportConfig.Builder.init(this).setLogEnabled(true).apply();
-        ReportManager.authorizations("",null);
     }
 }
